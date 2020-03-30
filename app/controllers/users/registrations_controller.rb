e5b40f42ed349f3d1186c60ee95e9bd84fd884cb
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-03-30 00:54:33 -0700

    login complete

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  # before_action :one_user_registered?, only: [:new, :create]
+  before_action :one_user_registered?, only: [:new, :create]
 
   # GET /resource/sign_up
   def new
