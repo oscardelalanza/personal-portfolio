f8b0670c61f92c6f893aa78ddffb3acf61f98d82
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-04 01:30:22 -0700

    validations added to personal_info model

--- app/models/personal_info.rb
+++ app/models/personal_info.rb
@@ -1,3 +1,9 @@
 class PersonalInfo < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true, uniqueness: true
+  validates :full_name, presence: true
+  validates :birth_date, presence: true
+  validates :description, allow_blank: true
+  validates :profile_img, allow_blank: true
 end
