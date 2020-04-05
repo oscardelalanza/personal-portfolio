35c3a0d7242c422401f5cfc788f85259cbf0251f
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-05 13:58:54 -0700

    personal_info validations fixed

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ group :development, :test do
 end
 
 group :development do
+  # debugger gem
+  gem 'byebug'
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
--- app/controllers/personal_info_controller.rb
+++ app/controllers/personal_info_controller.rb
@@ -4,8 +4,19 @@ class PersonalInfoController < ApplicationController
   def show; end
 
   def new
-    @personal_info = current_user.build_personal_info
+    @personal_info = PersonalInfo.new
   end
 
-  def create; end
+  def create
+    @personal_info = current_user.personal_info.build(personal_info_params)
+    @personal_info.save
+
+    redirect_to personal_info_path(current_user)
+  end
+
+  private
+
+  def personal_info_params
+    params.require(:personal_info).permit(:full_name, :birth_date, :nationality, :description)
+  end
 end
