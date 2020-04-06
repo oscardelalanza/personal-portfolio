56afe233b1c858a6b4781dbfc7bfbf051b3a86e0
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-05 20:37:03 -0700

    personal info now is rendered

--- app/controllers/personal_info_controller.rb
+++ app/controllers/personal_info_controller.rb
@@ -1,7 +1,11 @@
 class PersonalInfoController < ApplicationController
   layout 'admin'
 
-  def show; end
+  def show
+    @personal_info = PersonalInfo.find_by(user_id: params[:id])
+
+    @personal_info = PersonalInfo.new if @personal_info.nil?
+  end
 
   def new
     @personal_info = PersonalInfo.new
@@ -10,9 +14,7 @@ class PersonalInfoController < ApplicationController
   def create
     @personal_info = current_user.build_personal_info(personal_info_params)
 
-    if @personal_info.save
-      redirect_to personal_info_path(current_user)
-    end
+    redirect_to personal_info_path(current_user) if @personal_info.save
   end
 
   private
