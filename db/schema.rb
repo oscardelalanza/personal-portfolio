3a6a517e2e165c0e5ca5326863a64449c4a825ae
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-07 19:11:49 -0700

    profile_img removed from personal_infos, now is handled for active record

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_015612) do
+ActiveRecord::Schema.define(version: 2020_04_08_020647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2020_04_08_015612) do
     t.date "birth_date", null: false
     t.string "nationality", default: "", null: false
     t.text "description", default: ""
-    t.string "profile_img", default: ""
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_personal_infos_on_user_id", unique: true
