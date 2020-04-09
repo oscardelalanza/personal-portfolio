ecf323a20fc1bc99e1be7cf9869a6d07f420d393
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-08 19:36:54 -0700

    education migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_020647) do
+ActiveRecord::Schema.define(version: 2020_04_09_020053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,19 @@ ActiveRecord::Schema.define(version: 2020_04_08_020647) do
     t.index ["user_id"], name: "index_contact_infos_on_user_id"
   end
 
+  create_table "educations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", default: "", null: false
+    t.string "school", default: "", null: false
+    t.text "description", default: ""
+    t.string "url", default: ""
+    t.date "start"
+    t.date "end"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_educations_on_user_id"
+  end
+
   create_table "personal_infos", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "full_name", default: "", null: false
@@ -73,5 +86,6 @@ ActiveRecord::Schema.define(version: 2020_04_08_020647) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contact_infos", "users"
+  add_foreign_key "educations", "users"
   add_foreign_key "personal_infos", "users"
 end
