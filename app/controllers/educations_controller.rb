5da8e17c71304a60974f42763f969c048dc90130
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-09 16:52:13 -0700

    education form added

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,5 +2,11 @@ class EducationsController < ApplicationController
   layout 'admin'
   before_action :authenticate_user!
 
-  def show; end
+  def show
+    @educations = Education.where(user_id: params[:id])
+  end
+
+  def new
+    @education = Education.new
+  end
 end
