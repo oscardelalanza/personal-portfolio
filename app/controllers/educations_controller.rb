c7272ca1190f257b679fd48a176d0d87839f11ba
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-09 22:13:27 -0700

    education create added

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -9,4 +9,19 @@ class EducationsController < ApplicationController
   def new
     @education = Education.new
   end
+
+  def create
+    @education = current_user.educations.build(education_params.except(:certificate))
+
+    if @education.save
+      @education.certificate.attach(education_params[:certificate]) unless education_params[:certificate].nil?
+      redirect_to education_path(current_user)
+    end
+  end
+
+  private
+
+  def education_params
+    params.require(:education).permit(:title, :school, :description, :url, :start, :end, :certificate)
+  end
 end
