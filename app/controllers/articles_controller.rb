d220fbb355808b6f140728b05a2b3f04df7758a6
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-13 14:06:56 -0700

    articles create added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,15 @@ class ArticlesController < ApplicationController
   def show
     @new_article = Article.new
   end
+
+  def create
+    @new_article = current_user.articles.build(article_params)
+    redirect_to article_path(current_user) if @new_article.save
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:url)
+  end
 end
