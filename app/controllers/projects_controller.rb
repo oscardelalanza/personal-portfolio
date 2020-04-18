85b1c56b42bdba00cb1ec31a5a05dbd372fdd667
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-04-17 22:23:45 -0700

    project create added

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,15 @@ class ProjectsController < ApplicationController
   def new
     @new_project = Project.new
   end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    redirect_to projects_path if @project.save
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :repository, :demo, :description, :content)
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
- class Project < ApplicationRecord
+class Project < ApplicationRecord
   belongs_to :user
   has_rich_text :content
 end
