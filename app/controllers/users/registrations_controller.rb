749f894c526d9dc8ac39f741781bda8b7feb3241
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-02 13:51:38 -0700

    devise views restored

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'session'
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  before_action :one_user_registered?, only: [:new, :create]
+  # before_action :one_user_registered?, only: [:new, :create]
 
   # GET /resource/sign_up
   def new
