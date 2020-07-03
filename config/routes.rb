28ba193be32534645c6ff019865c79802a5f228f
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-03 15:04:59 -0700

    registration styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', controllers: { registrations: 'users/registrations', sessions: 'users/sessions' },
-             path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'just_for_the_owner' }
+             path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: '' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 
