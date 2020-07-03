1cf29ba5c3d63dc143613a626e616480d44beb96
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-03 15:26:40 -0700

    before_action comment removed'

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   layout 'session'
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  # before_action :one_user_registered?, only: [:new, :create]
+  before_action :one_user_registered?, only: [:new, :create]
 
   # GET /resource/sign_up
   def new
