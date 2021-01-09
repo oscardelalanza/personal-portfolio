148115127e722704e8fa7c7582c62174e7356a42
Oscar De La Lanza <oscardelalanza@gmail.com>
2021-01-08 16:37:10 -0800

    devise user generated and dashboard controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
